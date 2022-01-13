3e386908f206fa3164861f1ba1a0701e3b4839d1
Adrienne Meltzer <you@example.com>
2022-01-13 00:58:13 -0500

    Chapter 4 Exercises Done

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+
+    private Teacher professor;
+    private int courseCredits;
+    private ArrayList <String> materials;
+    private HashMap <Student, Integer> studentAttendance;
+}
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,22 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student adrienne = new Student();
+        adrienne.setName("Adrienne");
+        adrienne.setStudentId(6);
+        adrienne.setNumberOfCredits(1);
+        adrienne.setGpa(4.0);
+
+        Student nick = new Student("Nick", 7, 4,3.0);
+
+        System.out.println("Student name: " + adrienne.getName());
+        System.out.println("Student ID: " + adrienne.getStudentId());
+        System.out.println("Number of Credits: " + adrienne.getNumberOfCredits());
+        System.out.println("Current GPA: " + adrienne.getGpa());
+
+        System.out.println("Student name: " + nick.getName());
+        System.out.println("Student ID: " + nick.getStudentId());
+        System.out.println("Number of Credits: " + nick.getNumberOfCredits());
+        System.out.println("Current GPA: " + nick.getGpa());
     }
 }
